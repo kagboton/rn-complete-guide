6664961c97923154168ad4c2d94a8bf0fc759c19
Kilian Agboton <kilian.agboton@gmail.com>
2020-09-07 22:26:21 -0400

    some styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -22,8 +22,14 @@ const GoalInput = props => {
                     onChangeText= {goalInputHandler} 
                     value = {enteredGoal}
                 />
-                <Button title="CANCEL" color='red' onPress = {props.onCancelGoal}/>
-                <Button title="ADD" onPress = {addGoalHandler}/>
+                <View style = {styles.buttonContainer}>
+                    <View style = {styles.button}>
+                        <Button title="CANCEL" color='red' onPress = {props.onCancelGoal}/>
+                    </View>
+                    <View style = {styles.button}>
+                        <Button title="ADD" onPress = {addGoalHandler}/>
+                    </View>
+                </View>
             </View>
 
         </Modal>
@@ -42,6 +48,14 @@ const styles = StyleSheet.create({
         borderWidth: 1, 
         padding: 10,
         marginBottom : 10
-    } 
+    }, 
+    buttonContainer:{
+        flexDirection: 'row',
+        justifyContent : 'space-around',
+        width: '60%' 
+    },
+    button : {
+        width : '40%'
+    }
 })
 export default GoalInput
